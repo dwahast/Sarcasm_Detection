98cc2a81bd09f0e9c0dc87f9bea5c2d39e439bad
Douglas <douglaswahastdacosta@gmail.com>
2019-06-19 15:08:28 -0300

    new column "in_reply"

--- twitter_stream.py
+++ twitter_stream.py
@@ -14,75 +14,44 @@ access_token_secret = "W3c7GbaU3S0wH2c5yXcniL3T8z42qADboaCR1WGDoblvh"
 consumer_key = "gMChjJwVfA3FroAqExIqiUQfg"
 consumer_secret = "flMX4koxYMpIbHvBhgX8iYpJ5MGxpfN1InEqs4RUfOUQK4ooYK"
 
-#if file exists open the file and read for append new rows
+#This handles Twitter authetification and the connection to Twitter Streaming API
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = API(auth)
+
+#if file exists
 if(os.path.isfile('twitter_sarcastic_data.csv')):
+    print("CSV Already exists.\n reading...")
     dataTwitter = pd.read_csv('twitter_sarcastic_data.csv')
-    dataTwitter = dataTwitter[['text', 'is_sarcastic']]
+    dataTwitter = dataTwitter[['text', 'in_reply', 'is_sarcastic']]
 else:
-    dataTwitter = pd.DataFrame(columns=['text','is_sarcastic'])
+    print("CSV don't exists, new Data Frame created.\n")
+    dataTwitter = pd.DataFrame(columns=['text','in_reply','is_sarcastic'])
 
+#print current dataframe
+print("Current DataFrame( size = ", len(dataTwitter),'):')
 print(dataTwitter.tail(10))
-print('size of DataFrame:',len(dataTwitter))
+old_size_df = len(dataTwitter)
 
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
-    def __init__(self, api=None):
-        super(StdOutListener, self).__init__()
-        self.num_tweets = 0
+#main code, reading tweets and get their text and the reply text if were a reply.
+for tweet in tweepy.Cursor(api.search,q=["#sqn","#SQN","sqn","SQN"],count=10,tweet_mode="extended",result_type="recent",include_entities=True,lang="pt",locale='BR').items():
+    print("\nTWEET:\n", tweet.full_text)
+    if(tweet.in_reply_to_status_id != None):
+        try:
+            print("IN REPLY TO:\n", api.get_status(tweet.in_reply_to_status_id, tweet_mode="extended").full_text)
+            dataTwitter.loc[len(dataTwitter)] = [tweet.full_text, api.get_status(tweet.in_reply_to_status_id, tweet_mode="extended").full_text, 1]    
+           
+        except:
+            dataTwitter.loc[len(dataTwitter)] = [tweet.full_text, '', 1]    
+    else:
+        dataTwitter.loc[len(dataTwitter)] = [tweet.full_text, '', 1]    
+    print('\n-------------------------------------------------------------------')
 
-    def on_data(self, data):
-        global dataTwitter
-        tweet = json.loads(data)
-        if('extended_tweet' in tweet):
-            print("Extended Tweet:")
-            print (tweet['extended_tweet']['full_text'])
-            tweet = [(tweet['extended_tweet']['full_text'])]
-        else:
-            print("Normal Tweet")
-            print (tweet['text'])
-            tweet = [(tweet['text'])]
-        #print(type(tweet))
-        self.num_tweets += 1
-        dataTwitter.loc[len(dataTwitter)] = [''.join(tweet), 1]
-        #print(dataTwitter.sample())
+#print new dataframe
+print("Data Frame after this execution:")       
+print(dataTwitter.tail())
+print("Added: ",len(dataTwitter) - old_size_df)
 
-        if(self.num_tweets <= 3):
-            print('Success: ', self.num_tweets)
-            return True
-        else:
-            return False
-
-    def on_error(self, status):
-        print ("error", status)
-
-
-if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = API(auth)
-    #stream = Stream(auth, l, tweet_mode="extended")
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    #stream.filter(track=['SQN','sqn', '#SQN','#sqn'], languages=['pt'])
-    #searched_tweets = api.search(q='#sqn', lang='pt', locale='BR', count=500, tweet_mode="extended")
-    for tweet in tweepy.Cursor(api.search,q=["#sqn","#SQN","sqn","SQN"],count=2,tweet_mode="extended",result_type="recent",include_entities=True,lang="pt",locale='BR').items():
-        print("\n\nTWEET(text):\n", tweet.full_text)
-        if(tweet.in_reply_to_status_id != None):
-            print("EM RESPOSTA A:\n", api.get_status(tweet.in_reply_to_status_id, tweet_mode="extended").full_text, "\n\n")
-    #write the df to csv file
-    #dataTwitter.to_csv('twitter_sarcastic_data.csv', index=False)#
-
-    #print(dataTwitter.tail())
-
-    #tweets_data = []
-
-    #for i, tweet in enumerate(searched_tweets):
-    #    tweets_data.append(tweet._json)
-    #    print("Tweet", i,": \n")
-    #    print(tweets_data[i]['full_text'])
-    #    print("\n")
-
-    #print(tweets_data)
+#write the df to csv file
+dataTwitter.to_csv('twitter_sarcastic_data.csv', index=False)#
+print("Sucessul Writing CSV to file")
