fd4a1e6e217eac37055ec66d82a72540bde594b6
Douglas Wahast da Costa <douglaswahastdacosta@pop-os.localdomain>
2019-08-06 17:35:35 -0300

    manage

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@ import pandas as pd
 import os
 import json
 
-df = pd.read_csv('not_sarcastic_data.csv')
+df = pd.read_csv('sarcastic_data.csv')
+sb = len(df)
+print("tam before:" , sb)
+df = df.drop_duplicates(['text'],keep='last')
+print("Duplicated data Removed: ",sb - len(df))
+#print new dataframe
+print("Data Frame after this execution:")
+print(df.tail())
 #print current dataframe
 # print("Current DataFrame( size = ", len(dataTwitter),'):')
 # print(dataTwitter['in_reply'])
